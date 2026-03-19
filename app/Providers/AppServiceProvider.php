a33fecf1dc29ee32eac8aba6c5adb12acd465ac2
AnaVukajlovic <avukajlovic542@gmail.com>
2026-03-19 17:21:59 +0100

    remove artisan calls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,12 +11,8 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-public function boot(): void
-{
-    \Illuminate\Support\Facades\Artisan::call('config:clear');
-    \Illuminate\Support\Facades\Artisan::call('cache:clear');
-    \Illuminate\Support\Facades\Artisan::call('route:clear');
-    \Illuminate\Support\Facades\Artisan::call('view:clear');
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-}
+    public function boot(): void
+    {
+        //
+    }
 }
