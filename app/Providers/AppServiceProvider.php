1342870209e5eaba01aa70c3a77cb75105b6dde7
AnaVukajlovic <avukajlovic542@gmail.com>
2026-03-19 16:48:27 +0100

    Run migrations in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (App::environment('production')) {
+            Artisan::call('migrate', ['--force' => true]);
+        }
     }
 }
